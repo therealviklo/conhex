60f6f746f5a17aa75f4b0d3289fde55f4dd1cb2e
viklo <viggo@gstromberg.nu>
2020-07-02 17:40:28 +0200

    Hämtade mer saker från wavmusic

---
+++ menu.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "textutils.h"
+#include "textdisp.h"
+#include "textimage.h"
+#include "timer.h"
+#include <vector>
+#include <string>
+#include <memory>
+
+struct MenuResult
+{
+	bool successful;
+	std::string menu;
+	std::string option;
+};
+
+class Menu
+{
+public:
+	typedef std::vector<std::pair<std::string, std::vector<std::string>>> options_t;
+private:
+	options_t options;
+	int menuSelectedOption;
+	int listSelectedOption;
+	bool isInMenu;
+
+	Box getCoveredArea(int x, int y);
+public:
+	Menu(options_t options);
+
+	bool inMenu() {return isInMenu;}
+
+	void draw(int x, int y, Textdisp& textdisp);    
+	MenuResult enterMenu(int x, int y, Textdisp& textdisp, Timer& timer);
+
+	bool moveCursor(bool wPressed, bool aPressed, bool sPressed, bool dPressed);
+};

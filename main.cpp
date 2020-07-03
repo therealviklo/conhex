f1e37447ecb3aef84f88b5c13180a59feb1c723b
viklo <viggo@gstromberg.nu>
2020-07-03 14:14:45 +0200

    vars

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include "textdisp.h"
 #include "timer.h"
+#include "vars/gfx.h"
+
+std::unique_ptr<Textdisp> td;
+std::unique_ptr<Timer> timer;
 
 int main()
 {
-	Textdisp td;
-	Timer timer(60);
-
+	td = std::make_unique<Textdisp>();
+	timer = std::make_unique<Timer>(60);
+	
 	while (true)
 	{
-		td.clear();
+		td->clear();
 
-		td.set(1, 1, L'd', BackgroundColour::black | ForegroundColour::white);
+		td->set(1, 1, L'd', BackgroundColour::black | ForegroundColour::white);
 
-		td.draw();
+		td->draw();
 	}
 }
---
+++ vars/gfx.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <memory>
+#include "../textdisp.h"
+#include "../timer.h"
+
+extern std::unique_ptr<Textdisp> td;
+extern std::unique_ptr<Timer> timer;

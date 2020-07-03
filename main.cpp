4485b10712ef02b667a9d56d8fc376d1259e3f4e
viklo <viggo@gstromberg.nu>
2020-07-03 14:27:06 +0200

    input

--- main.cpp
+++ main.cpp
@@ -2,21 +2,39 @@
 #include "textdisp.h"
 #include "timer.h"
 #include "vars/gfx.h"
+#include "vars/input.h"
 
+// gfx
 std::unique_ptr<Textdisp> td;
 std::unique_ptr<Timer> timer;
+// input
+std::unique_ptr<Textinp> ti;
 
 int main()
 {
-	td = std::make_unique<Textdisp>();
-	timer = std::make_unique<Timer>(60);
-	
-	while (true)
+	try
 	{
-		td->clear();
+		// gfx
+		td = std::make_unique<Textdisp>();
+		timer = std::make_unique<Timer>(60);
+		// input
+		ti = std::make_unique<Textinp>();
+		
+		while (true)
+		{
+			td->clear();
 
-		td->set(1, 1, L'd', BackgroundColour::black | ForegroundColour::white);
+			td->set(1, 1, L'd', BackgroundColour::black | ForegroundColour::white);
 
-		td->draw();
+			td->draw();
+		}
+	}
+	catch (const std::exception& e)
+	{
+		MessageBoxA(NULL, e.what(), "Fel", MB_OK | MB_ICONERROR);
+	}
+	catch (...)
+	{
+		MessageBoxW(NULL, L"Okänt fel", L"Fel", MB_OK | MB_ICONERROR);
 	}
 }
---
+++ vars/input.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <memory>
+#include "../textdisp.h"
+
+extern std::unique_ptr<Textinp> ti;
